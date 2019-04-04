f1ffa19d63c9c6ac4f9f986071ac71cba7bb2992
ramzy27 <cellist27@gmail.com>
2019-04-04 22:15:35 +0200

    format simple number

---
+++ src/main/java/kata/FooBarQixCalculator.java
@@ -0,0 +1,10 @@
+package kata;
+
+public class FooBarQixCalculator {
+
+
+    public String calculateFooBarQix(int number) {
+        return String.valueOf(number);
+    }
+
+}
---
+++ src/test/java/kata/FooBarQixCalculatorTest.java
@@ -0,0 +1,23 @@
+package kata;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class FooBarQixCalculatorTest {
+
+    FooBarQixCalculator fooBarQixCalculator;
+
+    @Before
+    public void init() {
+        fooBarQixCalculator = new FooBarQixCalculator();
+    }
+
+    @Test
+    public void should_return_same_int() {
+        Assert.assertEquals("1", fooBarQixCalculator.calculateFooBarQix(1));
+    }
+
+
+}
